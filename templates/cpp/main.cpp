5352d315c47f8ce6c00836e32d40a9c240af8a75
plavelo <plastic.velocity@gmail.com>
2021-09-08 08:13:11 +0900

    update a cpp template

---
+++ templates/cpp/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <boost/multi_array.hpp>
+#include <boost/optional.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/range/algorithm.hpp>
+#include <boost/range/adaptors.hpp>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+// clang-format off
+#pragma GCC target("avx")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+template <typename T> inline bool chmin(T& a, const T& b) { bool compare = a > b; if (a > b) a = b; return compare; }
+template <typename T> inline bool chmax(T& a, const T& b) { bool compare = a < b; if (a < b) a = b; return compare; }
+// clang-format on
+
+void logic()
+{
+    // write your code here
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    logic();
+    return 0;
+}
